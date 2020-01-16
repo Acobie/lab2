194dc9545c7bffa087cbce39c572b7676e1d429c
Acobie <angel.c.obie@gmail.com>
2020-01-16 10:05:58 -0800

    Update introHCI.js

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -6,4 +6,5 @@ function initializePage() {
     $('#testjs').click(function(e) {
             $('.jumbotron h1').text("Javascript is connected");
             });
+    $("a.thumbnail").click(projectClick);
 }
